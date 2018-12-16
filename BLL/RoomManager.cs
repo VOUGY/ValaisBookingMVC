1cf352cebeb896ecd6aecda1cf47058ec54bbc2e
VOUGY <guillaume.vouilloz@outlook.com>
2018-12-16 19:12:56 +0100

    Update RoomManager.cs

--- BLL/RoomManager.cs
+++ BLL/RoomManager.cs
@@ -5,19 +5,26 @@ using System.Text;
 using System.Threading.Tasks;
 using DTO;
 using DAL;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace BLL
 {
     public class RoomManager
     {
+        static string baseUri = "http://localhost:1176/api/";
+
         /// <summary>
         /// Return a list of Room who has no reservation during a defined period and in relation with a hotel
         /// </summary>
         public static List<Room> GetRoomsWithNoReservationByHotel(int idHotel, DateTime arrival, DateTime departure)
         {
-            List<Room> rooms = RoomDB.GetRoomsWithNoReservationByHotel(idHotel, arrival, departure);
-
-            return rooms;
+            string uri = baseUri + "rooms";
+            using (HttpClient httpClient = new HttpClient())
+            {
+                Task<String> response = httpClient.GetStringAsync(uri);
+                return JsonConvert.DeserializeObject<List<Room>>(response.Result);
+            }
         }
 
         /// <summary>
@@ -25,9 +32,12 @@ namespace BLL
         /// </summary>
         public static Room GetRoom(int idRoom)
         {
-            Room room = RoomDB.GetRoom(idRoom);
-
-            return room;
+            string uri = baseUri + "rooms/" + idRoom;
+            using (HttpClient httpClient = new HttpClient())
+            {
+                Task<String> response = httpClient.GetStringAsync(uri);
+                return JsonConvert.DeserializeObject<Room>(response.Result);
+            }
         }
 
         /// <summary>
